dbf840969143a4f7b9e53829c6e763c29ab97fd9
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-26 14:19:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+document.querySelector('form').addEventListener('submit', function(event) {
+  // Prevent the form from submitting
+  event.preventDefault();
+
+  // Get the current values of the inputs
+  let fontsize = document.getElementById('fontsize').value;
+  let fontcolor = document.getElementById('fontcolor').value;
+
+  // Save the values as cookies
+  document.cookie = "fontsize=fontsize; fontcolor=fontcolor";
+});
